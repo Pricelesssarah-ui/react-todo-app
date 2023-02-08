51e0ffc27c15a932be58c009c589e8f663159020
Pricelesssarah-ui <pricelesssarah3@gmail.com>
2023-02-08 17:43:14 +0100

    My react todo list app

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import "./App.css";
+
+const App = () => {
+
+
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
+
+
+
+  //handleSubmit function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+
+
+    if(editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) => t.id === editTodo.id ? 
+      (t={id:t.id, todo}): {id:t.id, todo:t.todo}
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+
+
+
+    if(todo !== "") {
+      setTodos([{ id:`${todo} = ${Date.now()}` ,todo }, ...todos]);
+      setTodo("");
+    }
+
+  };
+
+
+// handleDelete function
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos([...delTodo]);
+
+  }
+
+
+// handleEdit function
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  };
+
+
+
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>To-do List App</h1>
+
+        <form className='todoForm' onSubmit={handleSubmit}>
+
+          <input type="text" 
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)} 
+          />
+
+          <button type='submit'> {editId?"Edit" : "ADD"} </button>
+        </form>
+
+
+
+
+        <ul className='todos'>
+
+
+          {
+            todos.map((t) => (
+
+            <li className='todoList'>
+            <span className='todoText' key={t.id}>{t.todo}</span>
+            <button onClick={() => handleEdit(t.id)}>Edit</button>
+            <button onClick={() => handleDelete(t.id)}>Delete</button>
+          </li>
+          ))
+          }
+
+        </ul>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
